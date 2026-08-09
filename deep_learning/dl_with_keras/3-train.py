aaa5df33a1d1c9f748a5394c297fb1b2bff9f692
nickygrey <sgp5vb2qjd@privaterelay.appleid.com>
2026-08-09 09:22:37 +0200

    Add train_model function for Keras models

---
+++ deep_learning/dl_with_keras/3-train.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Module to train a Keras model."""
+from tensorflow import keras
+
+
+def train_model(model, X, Y, epochs, verbose=1):
+    """Train a Keras model on input data and labels.
+
+    Args:
+        model: Keras model instance to train.
+        X: Input data array.
+        Y: Target labels array.
+        epochs (int): Number of training epochs.
+        verbose (int): Verbosity mode (0 = silent, 1 = progress bar).
+
+    Returns:
+        None
+    """
+    model.fit(X, Y, epochs=epochs, verbose=verbose)
