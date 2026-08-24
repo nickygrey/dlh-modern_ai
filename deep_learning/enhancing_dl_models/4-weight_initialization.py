98f407e6f5dfddb94db89e304fb2e014c9540fef
nickygrey <sgp5vb2qjd@privaterelay.appleid.com>
2026-08-24 10:03:52 +0200

    Add build_model_initializer_by_activation function with Glorot and He initializers

---
+++ deep_learning/enhancing_dl_models/4-weight_initialization.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Module to build Keras model with activation-based weight initialization."""
+from tensorflow import keras
+
+
+def build_model_initializer_by_activation(input_dim, hidden_units, activation):
+    """Build a model with appropriate weight initializers for activations.
+
+    Args:
+        input_dim (int): Number of input features.
+        hidden_units (int): Number of neurons in the hidden layer.
+        activation (str): Activation function ('sigmoid', 'tanh',
+                          'relu', or 'leaky_relu').
+
+    Returns:
+        keras.Model: Keras model with configured hidden and output layers.
+    """
+    if activation in ['sigmoid', 'tanh']:
+        initializer = 'glorot_uniform'
+    elif activation in ['relu', 'leaky_relu']:
+        initializer = 'he_normal'
+    else:
+        initializer = 'glorot_uniform'
+
+    model = keras.Sequential([
+        keras.layers.Dense(
+            hidden_units,
+            activation=activation,
+            kernel_initializer=initializer,
+            input_shape=(input_dim,)
+        ),
+        keras.layers.Dense(10, activation='softmax')
+    ])
+
+    return model
